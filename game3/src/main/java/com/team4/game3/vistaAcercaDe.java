8f6ac2870f5f55411954e1ac5622e6bcd313e59d
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-04-04 12:25:26 +0200

    Vistas panelPrincipal y menus

---
+++ game3/src/main/java/com/team4/game3/vistaAcercaDe.java
@@ -0,0 +1,33 @@
+package com.team4.game3;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class vistaAcercaDe extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private JPanel contentPane;
+
+	public vistaAcercaDe() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setBounds(100, 100, 400, 200);
+		setTitle("Acerca De");
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel label1 = new JLabel(
+				"<html>MASTERMIND <br><br> Autores: Alberto del Pozo, Andrea de Pezzo y Roger Gibert <br><br> Más info??");
+		label1.setBounds(20, 24, 344, 113);
+		contentPane.add(label1);
+
+		setVisible(true);
+	}
+
+}
